e07d99d922e96eda13542b73680e1c06cee060ba
TheGoodBear <alain@micalaudie.com>
2023-01-02 16:54:01 +0100

    MAui sample v1

--- MauiSample/ViewModels/AnimalVM.cs
+++ MauiSample/ViewModels/AnimalVM.cs
@@ -6,6 +6,9 @@ namespace MauiSample.ViewModels;
 internal class AnimalVM
 {
 
+    private static List<Tuple<string, string>> AnimalPool = 
+        new List<Tuple<string, string>>();
+
     public string Title { get; set; }
 
     public Collection<Animal> Animals { get; set; }
@@ -13,11 +16,45 @@ internal class AnimalVM
 
     public AnimalVM()
     {
+
         Animal.InitializeData();
         Animals = Animal.Animals;
+        UpdateData();
+        CreateAnimalPool();
 
-		Title = $"Liste des {Animals.Count} animaux";
+    }
 
+
+    public void AddAnimal()
+    {
+
+        if (AnimalPool.Count > 0) 
+        {
+            new Animal(AnimalPool[0].Item1, AnimalPool[0].Item2);
+            AnimalPool.RemoveAt(0);
+            UpdateData();
+        }
+
+    }
+
+
+    private void UpdateData()
+    {
+
+        Title = $"Liste des {Animals.Count} animaux";
+
+    }
+
+    private void CreateAnimalPool()
+    {
+        AnimalPool.Add(new Tuple<string, string>
+            ("Dauphin", "Mammifère marin"));
+        AnimalPool.Add(new Tuple<string, string>
+            ("Lièvre", "Court vite"));
+        AnimalPool.Add(new Tuple<string, string>
+            ("Tortue", "Se déplace avec lenteur"));
+        AnimalPool.Add(new Tuple<string, string>
+            ("Pie", "Oiseau voleur"));
     }
 
 }
